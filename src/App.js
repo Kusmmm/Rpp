6b3e7e24d718992df85e6038689d5fbb990156dc
Ангелина Усачёва <>
2019-05-08 11:26:18 +0300

    добавлен Redux и структура папок хранилища

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
+//navbar
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+// cards
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 
+//left panel
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
