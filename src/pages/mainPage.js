f2370e129bb948ad53b65996c2f8e5530c7dfc3a
Ангелина Усачёва <>
2019-05-10 18:13:25 +0300

    макет расписания

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -9,7 +9,6 @@ import { withStyles } from '@material-ui/core/styles';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import theme from '../theme';
 
-
 // cards
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -22,188 +21,124 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-
-import Board from "react-trello";
-const data = {
-    "lanes": [
-        {
-            "id": "PLANNED",
-            "title": "21-05-2019",
-            "label": "",
-            "style": {
-                "width": 280
-            },
-            "cards": [
-                {
-                    "id": "Milk",
-                    "title": " Заголовок карточки 1",
-                    "label": "15 mins",
-                    "description": "Описание карточки",
-                    "people": "123"
-                },
-                {
-                    "id": "Plan2",
-                    "title": "Dispose Garbage",
-                    "label": "10 mins",
-                    "description": "Sort out recyclable and waste as needed"
-                },
-                {
-                    "id": "Plan3",
-                    "title": "Сделать пост",
-                    "label": "30 mins",
-                    "description": "Can AI make memes?"
-                },
-                {
-                    "id": "Plan4",
-                    "title": "Сделать бд",
-                    "label": "5 mins",
-                    "description": "рыолрвафлорвылфо"
-                }
-            ]
-        },
-        {
-            "id": "WIP",
-            "title": "Сделать рапапа",
-            "label": "Описание",
-            "style": {
-                "width": 280
-            },
-            "cards": [
-                {
-                    "id": "Wip1",
-                    "title": "ПРодать душу дьяволу",
-                    "label": "30 mins",
-                    "description": "Получаем все лабы и идём сдавать"
-                }
-            ]
-        },
-        {
-            "id": "BLOCKED",
-            "title": "Получаем люлей",
-            "label": "0/0",
-            "style": {
-                "width": 280
-            },
-            "cards": []
-        },
-        {
-            "id": "COMPLETED",
-            "title": "Осознаём ошибку и делаем всё заново",
-            "style": {
-                "width": 280
-            },
-            "label": "2/5",
-            "cards": [
-                {
-                    "id": "Completed1",
-                    "title": "Мяяяя",
-                    "label": "15 mins",
-                    "description": "Привет от Сани"
-                },
-                {
-                    "id": "Completed2",
-                    "title": "Хочу спатеньки",
-                    "label": "15 mins",
-                    "description": "Но надо делать дз"
-                }
-            ]
-        },
-        {
-            "id": "REPEAT",
-            "title": "Repeat",
-            "style": {
-                "width": 280
-            },
-            "label": "1/1",
-            "cards": [
-                {
-                    "id": "Repeat1",
-                    "title": "Сходить к ветеринару",
-                    "label": "30 mins",
-                    "description": "Научиться гавкать"
-                }
-            ]
-        },
-        {
-            "id": "ARCHIVED",
-            "title": "Archived",
-            "style": {
-                "width": 280
-            },
-            "label": "1/1",
-            "cards": [
-                {
-                    "id": "Archived1",
-                    "title": "Поиграть",
-                    "label": "300 mins",
-                    "description": "На нервах"
-                }
-            ]
-        },
-        {
-            "id": "ARCHIVED2",
-            "title": "Делать дз",
-            "style": {
-                "width": 280
-            },
-            "label": "1/1",
-            "cards": [
-                {
-                    "id": "Archived2",
-                    "title": "Убить Артёма",
-                    "label": "300 mins",
-                    "description": "холодным оружием"
-                }
-            ]
-        },
-        {
-            "id": "ARCHIVED3",
-            "title": "Archived3",
-            "style": {
-                "width": 280
-            },
-            "label": "1/1",
-            "cards": [
-                {
-                    "id": "Archived3",
-                    "title": "выкупит душу",
-                    "label": "300 mins",
-                    "description": "обратно"
-                }
-            ]
-        }
-    ]
-}
-
-const handleDragStart = (cardId, laneId) => {
-    console.log('drag started')
-    console.log(`cardId: ${cardId}`)
-    console.log(`laneId: ${laneId}`)
-}
-
-const handleDragEnd = (cardId, sourceLaneId, targetLaneId) => {
-    console.log('drag ended')
-    console.log(`cardId: ${cardId}`)
-    console.log(`sourceLaneId: ${sourceLaneId}`)
-    console.log(`targetLaneId: ${targetLaneId}`)
-}
-
-const styles = {
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginLeft: -18,
-        marginRight: 10,
-    },
-};
+import Scheduler, { SchedulerData, ViewTypes, DATE_FORMAT } from 'react-big-scheduler';
+//include `react-big-scheduler/lib/css/style.css` for styles, link it in html or import it here 
+import 'react-big-scheduler/lib/css/style.css';
+import moment from 'moment';
+import withDragDropContext from '../components/withDnDContext';
 
 
 
 @inject('store')
 @observer
-export default class MainPage extends Component {
+class MainPage extends Component {
+    constructor(props) {
+        super(props);
 
+        let DemoData = {
+            resources: [
+                {
+                    id: 'r0',
+                    name: 'Мои задачи',
+                    //groupOnly: true
+                }
+            ],
+            events: [
+                {
+                    id: 1,
+                    start: '2019-05-10 09:30:00',
+                    end: '2019-05-10 10:30:00',
+                    resourceId: 'r0',
+                    title: 'хоршая задача',
+                    bgColor: '#74D971',
+                },
+                {
+                    id: 4,
+                    start: '2019-05-05 09:30:00',
+                    end: '2019-05-06 8:30:00',
+                    resourceId: 'r0',
+                    title: 'тест тест тест',
+                    bgColor: '#5BC0EB',
+                    //showPopover: false
+                },
+                {
+                    id: 5,
+                    start: '2019-05-05 09:30:00',
+                    end: '2019-05-06 8:30:00',
+                    resourceId: 'r0',
+                    title: 'тест fndskj тест',
+                    bgColor: '#5BC0EB',
+                    //showPopover: false
+                },
+                
+                {
+                    id: 2,
+                    start: '2019-05-05 09:30:00',
+                    end: '2019-05-15 8:30:00',
+                    resourceId: 'r0',
+                    title: 'тест тест тест',
+                    bgColor: '#FDE74C',
+                    //showPopover: false
+                }
+                ,{
+                    id: 3,
+                    start: '2019-05-01 09:30:00',
+                    end: '2019-05-08 11:30:00',
+                    resourceId: 'r0',
+                    title: 'плохая задача',
+                    bgColor: '#DD5B3C',
+                    //showPopover: false
+                },
+                {
+                    id: 6,
+                    start: '2019-05-06 09:30:00',
+                    end: '2019-05-08 8:30:00',
+                    resourceId: 'r0',
+                    title: 'тест vsdp тест',
+                    bgColor: '#5BC0EB',
+                    //showPopover: false
+                },
+            ]
+
+        }
+        this.state = {
+            DemoData: DemoData
+        }
+    }
+    prevClick = (schedulerData) => {
+        schedulerData.prev();
+        schedulerData.setEvents(this.state.DemoData.events);
+        this.setState({
+            viewModel: schedulerData
+        })
+    }
+
+    nextClick = (schedulerData) => {
+        schedulerData.next();
+        schedulerData.setEvents(this.state.DemoData.events);
+        this.setState({
+            viewModel: schedulerData
+        })
+    }
+
+    onViewChange = (schedulerData, view) => {
+        schedulerData.setViewType(view.viewType, view.showAgenda, view.isEventPerspective);
+        schedulerData.setEvents(this.state.DemoData.events);
+        this.setState({
+            viewModel: schedulerData
+        })
+    }
+
+    onSelectDate = (schedulerData, date) => {
+        schedulerData.setDate(date);
+        schedulerData.setEvents(this.state.DemoData.events);
+        this.setState({
+            viewModel: schedulerData
+        })
+    }
+    eventItemClick = (schedulerData, event) => {
+        alert(`You just clicked an event: {id: ${event.id}, title: ${event.title}}`);
+    };
 
     login() {
         console.log('login...');
@@ -220,16 +155,30 @@ export default class MainPage extends Component {
         } else {
 
         }
-
-
     }
     render() {
+        let schedulerData = new SchedulerData(new moment().format(DATE_FORMAT), ViewTypes.Week,false, false, undefined);
+        //set locale moment to the schedulerData, if your locale isn't English. By default, Scheduler comes with English(en, United States).
+        moment.locale('ru');
+        schedulerData.setLocaleMoment(moment);
+        //set resources here or later
+        let resources = [
+            {
+                id: 'r0',
+                name: 'Мои задачи',
+                groupOnly: true
+            }
+        ];
+        schedulerData.setResources(resources);
+        //set events here or later, 
+        //the event array should be sorted in ascending order by event.start property, otherwise there will be some rendering errors
+        schedulerData.setEvents(this.state.DemoData.events);
 
         return (
             <MuiThemeProvider theme={theme}>
                 <AppBar position="static">
                     <Toolbar variant="dense">
-                        <IconButton style={styles.menuButton} color="inherit" aria-label="Menu">
+                        <IconButton color="inherit" aria-label="Menu">
                             <MenuIcon />
                         </IconButton>
                         <Typography variant="h6" color="inherit">
@@ -240,22 +189,22 @@ export default class MainPage extends Component {
                 </AppBar>
                 <AppBar position="static" color="inherit">
                     <Toolbar variant="dense">
-                        
+
                         <Button color="inherit">Создать задачу</Button>
                         <Button color="inherit">Создать колонку</Button>
                     </Toolbar>
                 </AppBar>
 
-
-                <div className="board-container">
-                    <Board data={data}
-                        draggable
-                        handleDragStart={handleDragStart}
-                        handleDragEnd={handleDragEnd}
-                    />
-                </div>
+                <Scheduler schedulerData={schedulerData}
+                    prevClick={this.prevClick}
+                    nextClick={this.nextClick}
+                    onSelectDate={this.onSelectDate}
+                    onViewChange={this.onViewChange}
+                    eventItemClick={this.eventClicked}
+                />
 
             </MuiThemeProvider>
         )
     }
 }
+export default withDragDropContext(MainPage)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
     context: __dirname,
     entry: './src/index.js',
@@ -27,13 +29,25 @@ module.exports = {
                 test: /\.jsx$/,
                 exclude: /(node_modules|bower_components)/,
                 loaders: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
             }
+
         ]
     },
     plugins: [
+
         new HtmlWebPackPlugin({
             template: path.resolve(__dirname, 'public/index.html'),
             filename: 'index.html'
+        }), new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+            chunkFilename: "[id].[contenthash].css"
         })
     ]
 };
